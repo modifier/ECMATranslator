1f2a71ce52d9ff16df02acc5282ad6907758247d
modifier <the.modifikator@gmail.com>
2014-05-11 17:29:56 +0400

    Updated grammar

--- src/org/modifier/ecmascript/SyntaxTable.java
+++ src/org/modifier/ecmascript/SyntaxTable.java
@@ -25,6 +25,14 @@ public class SyntaxTable extends AbstractSyntaxTable
             {
                 result.add(new TerminalNode(TokenClass.get("Literal")));
             }
+            else if (token.classId == TokenClass.get("Const"))
+            {
+                result.add(new TerminalNode(TokenClass.get("Const")));
+            }
+            else if (token.classId == TokenClass.get("Ident"))
+            {
+                result.add(new TerminalNode(TokenClass.get("Ident")));
+            }
             else if (token.value.equals("["))
             {
                 result.add(new NonTerminalNode(TokenClass.get("ArrayLiteral")));
@@ -32,6 +40,12 @@ public class SyntaxTable extends AbstractSyntaxTable
             else if (token.value.equals("{"))
             {
                 result.add(new NonTerminalNode(TokenClass.get("ObjectLiteral")));
+            }
+            else if (token.value.equals("("))
+            {
+                result.add(new TerminalNode("("));
+                result.add(new NonTerminalNode(TokenClass.get("Expression")));
+                result.add(new TerminalNode(")"));
             }
         }
         else if (nodeClass == TokenClass.get("ArrayLiteral"))
@@ -42,23 +56,14 @@ public class SyntaxTable extends AbstractSyntaxTable
         }
         else if (nodeClass == TokenClass.get("ElementList"))
         {
-            boolean fits;
-            try
-            {
-                fits = TokenClass.get("AssignmentExpression").fits(token);
-            }
-            catch (Exception e)
-            {
-                throw new SyntaxError();
-            }
+            result.add(new NonTerminalNode(TokenClass.get("AssignmentExpression")));
+            result.add(new NonTerminalNode(TokenClass.get("ElementList_1")));
+        }
+        else if (nodeClass == TokenClass.get("ElementList_1"))
+        {
             if (token.value.equals(","))
             {
                 result.add(new TerminalNode(","));
-                result.add(new NonTerminalNode(TokenClass.get("ElementList")));
-            }
-            else if (fits)
-            {
-                result.add(new NonTerminalNode(TokenClass.get("AssignmentExpression")));
                 result.add(new NonTerminalNode(TokenClass.get("ElementList")));
             }
         }
@@ -70,6 +75,11 @@ public class SyntaxTable extends AbstractSyntaxTable
         }
         else if (nodeClass == TokenClass.get("PropertyNameAndValueList"))
         {
+            if (token.value.equals("}"))
+            {
+                return result;
+            }
+
             result.add(new NonTerminalNode(TokenClass.get("PropertyName")));
             result.add(new TerminalNode(":"));
             result.add(new NonTerminalNode(TokenClass.get("AssignmentExpression")));
@@ -98,10 +108,124 @@ public class SyntaxTable extends AbstractSyntaxTable
                 throw new SyntaxError();
             }
         }
+        else if (nodeClass == TokenClass.get("Expression"))
+        {
+            result.add(new NonTerminalNode(TokenClass.get("AssignmentExpression")));
+            result.add(new NonTerminalNode(TokenClass.get("Expression_1")));
+        }
+        else if (nodeClass == TokenClass.get("Expression_1"))
+        {
+            if (token.value.equals(","))
+            {
+                result.add(new TerminalNode(","));
+                result.add(new NonTerminalNode(TokenClass.get("Expression")));
+            }
+        }
         else if (nodeClass == TokenClass.get("AssignmentExpression"))
         {
-            // TODO: temp
-            result.add(new TerminalNode(TokenClass.get("Ident")));
+            if (token.classId == TokenClass.get("AdditiveOperator") || token.classId == TokenClass.get("UnaryOperator"))
+            {
+                result.add(new NonTerminalNode(TokenClass.get("UnaryExpression")));
+            }
+            else
+            {
+                result.add(new NonTerminalNode(TokenClass.get("LeftHandSideExpression")));
+            }
+            result.add(new NonTerminalNode(TokenClass.get("BinaryExpression")));
+        }
+        else if (nodeClass == TokenClass.get("BinaryExpression"))
+        {
+            if (token.classId == TokenClass.get("AdditiveOperator"))
+            {
+                result.add(new TerminalNode(TokenClass.get("AdditiveOperator")));
+                result.add(new NonTerminalNode(TokenClass.get("UnaryExpression")));
+                result.add(new NonTerminalNode(TokenClass.get("BinaryExpression")));
+            }
+            else if (token.classId == TokenClass.get("BinaryOperator"))
+            {
+                result.add(new TerminalNode(TokenClass.get("BinaryOperator")));
+                result.add(new NonTerminalNode(TokenClass.get("UnaryExpression")));
+                result.add(new NonTerminalNode(TokenClass.get("BinaryExpression")));
+            }
+            else if (token.classId == TokenClass.get("AssignmentOperator"))
+            {
+                result.add(new TerminalNode(TokenClass.get("AssignmentOperator")));
+                result.add(new NonTerminalNode(TokenClass.get("AssignmentExpression")));
+                result.add(new NonTerminalNode(TokenClass.get("BinaryExpression")));
+            }
+        }
+        else if (nodeClass == TokenClass.get("UnaryExpression"))
+        {
+            if (token.classId == TokenClass.get("AdditiveOperator"))
+            {
+                result.add(new TerminalNode(TokenClass.get("AdditiveOperator")));
+            }
+            else if (token.classId == TokenClass.get("UnaryOperator"))
+            {
+                result.add(new TerminalNode(TokenClass.get("UnaryOperator")));
+            }
+            result.add(new NonTerminalNode(TokenClass.get("LeftHandSideExpression")));
+            result.add(new NonTerminalNode(TokenClass.get("PostfixExpression")));
+        }
+        else if (nodeClass == TokenClass.get("PostfixExpression"))
+        {
+            if (token.classId == TokenClass.get("PostfixOperator"))
+            {
+                result.add(new TerminalNode(TokenClass.get("PostfixOperator")));
+            }
+        }
+        else if (nodeClass == TokenClass.get("LeftHandSideExpression"))
+        {
+            result.add(new NonTerminalNode(TokenClass.get("MemberExpression")));
+            result.add(new NonTerminalNode(TokenClass.get("LeftHandSideExpression_1")));
+        }
+        else if (nodeClass == TokenClass.get("LeftHandSideExpression_1"))
+        {
+
+        }
+        else if (nodeClass == TokenClass.get("MemberExpression"))
+        {
+            if (token.value.equals("new"))
+            {
+                result.add(new NonTerminalNode(TokenClass.get("AllocationExpression")));
+            }
+            else if (token.value.equals("function"))
+            {
+                result.add(new NonTerminalNode(TokenClass.get("FunctionExpression")));
+            }
+            else
+            {
+                result.add(new NonTerminalNode(TokenClass.get("PrimaryExpression")));
+            }
+            result.add(new NonTerminalNode(TokenClass.get("MemberExpressionPart")));
+        }
+        else if (nodeClass == TokenClass.get("MemberExpressionPart"))
+        {
+            if (token.value.equals("("))
+            {
+                result.add(new TerminalNode("("));
+                result.add(new NonTerminalNode(TokenClass.get("Expression")));
+                result.add(new TerminalNode(")"));
+                result.add(new NonTerminalNode(TokenClass.get("MemberExpressionPart")));
+            }
+            else if (token.value.equals("["))
+            {
+                result.add(new TerminalNode("["));
+                result.add(new NonTerminalNode(TokenClass.get("Expression")));
+                result.add(new TerminalNode("]"));
+                result.add(new NonTerminalNode(TokenClass.get("MemberExpressionPart")));
+            }
+            else if (token.value.equals("."))
+            {
+                result.add(new TerminalNode("."));
+                result.add(new TerminalNode(TokenClass.get("Ident")));
+                result.add(new NonTerminalNode(TokenClass.get("MemberExpressionPart")));
+            }
+        }
+        else if (nodeClass == TokenClass.get("AllocationExpression"))
+        {
+            result.add(new TerminalNode("new"));
+            result.add(new NonTerminalNode(TokenClass.get("MemberExpression")));
         }
         else
         {
--- test/lexer/sample.js
+++ test/lexer/sample.js
@@ -1,5 +1,16 @@
 {
-    "test1": duck,
-    "test2": hits,
-    "test3": allelujah
+    "test1": duck + 100500,
+    "test2": hits / 255 + 55 - 255,
+    "test3": allelujah[3],
+    "test4": [literal1, "literal2", foo3],
+    "test5": [],
+    "test6": {
+        "key": "value"
+    },
+    "test7": new hits()[15],
+    "test8": [,],
+    "test9": {},
+    "test10": hits()[11],
+    "test11": (11 + 5) / 2,
+    "test12": new hits(arg1, arg2)[new shit(255 - 314 / 155)]
 }
